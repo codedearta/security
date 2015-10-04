74334f31a8dd1de5789719725752abbdd3e1b71d
codedearta <sepp.renfer@gmail.com>
2015-10-05 00:03:48 +0100

    angular added

--- examples/src/main/resources/assets/js/app.js
+++ examples/src/main/resources/assets/js/app.js
@@ -1,32 +1,30 @@
-/*global angular */
-
-/**
- * The main dearta security example app module
- *
- * @type {angular.Module}
- */
-angular.module('dearta', ['ngRoute'])
-	.config(function ($routeProvider) {
-		'use strict';
-
-		var routeConfig = {
-			controller: 'LoginCtrl',
-			templateUrl: 'todomvc-index.html',
-			resolve: {
-				store: function (todoStorage) {
-					// Get the correct module (API or localStorage).
-					return todoStorage.then(function (module) {
-						module.get(); // Fetch the todo records in the background.
-						return module;
-					});
-				}
-			}
-		};
-
-		$routeProvider
-			.when('/', routeConfig)
-			.when('/:status', routeConfig)
-			.otherwise({
-				redirectTo: '/'
-			});
-	});
+function ctrl($scope){
+  $scope.rows = ['Paul','John','Lucie'];
+  $scope.temp = false;
+  
+  $scope.addRow = function(){
+    $scope.temp = false;
+    $scope.addName="";
+  };
+  
+  $scope.deleteRow = function(row){
+    $scope.rows.splice($scope.rows.indexOf(row),1);
+  };
+  
+  $scope.plural = function (tab){
+    return tab.length > 1 ? 's': ''; 
+  };
+  
+  $scope.addTemp = function(){
+    if($scope.temp) $scope.rows.pop(); 
+    else if($scope.addName) $scope.temp = true;
+    
+    if($scope.addName) $scope.rows.push($scope.addName);
+    else $scope.temp = false;
+  };
+  
+  $scope.isTemp = function(i){
+    return i==$scope.rows.length-1 && $scope.temp;
+  };
+  
+}
