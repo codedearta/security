78ad7b66cf1dca9efa7a46d848a3779cc0dc6a02
codedearta <sepp.renfer@gmail.com>
2015-10-12 18:24:41 +0100

    pem loader added

--- gen_key.sh
+++ gen_key.sh
@@ -1,5 +1,4 @@
 openssl genrsa -out rsa/src/main/resources/private.pem 4096
 openssl rsa -in rsa/src/main/resources/private.pem -outform PEM -pubout -out rsa/src/main/resources/public.pem
-
 echo 'Hallo Decrypto lib' |
 openssl rsautl -encrypt -pubin -inkey rsa/src/main/resources/public.pem > cypher.dat
